ebbdf2cbef97883c9d1cf3fcc4af86fd060ddc46
morenoa <Amoreno3657@Adams-MacBook-Pro.local>
2017-01-29 10:01:48 -0600

    Initial commit

---
+++ app/src/main/java/net/androidbootcamp/healthyrecipes/Recipe.java
@@ -0,0 +1,20 @@
+package net.androidbootcamp.healthyrecipes;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Recipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+            }
+    }
